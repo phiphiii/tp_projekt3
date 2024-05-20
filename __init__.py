84e5f59a07754d60c8bccfc2c7fa01a7d962bb8d
phiphi <62543350+phiphiii@users.noreply.github.com>
2024-05-20 22:55:32 +0200

    Final version

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-from ._core import __doc__, __version__, generateSinus, chart_printing, generateSawtooth, generateRectangular, generateCosine
+from ._core import __doc__, __version__, generateSinus, chart_printing, generateSawtooth, generateRectangular, generateCosine, imageFiltration
 
-__all__ = ["__doc__", "__version__", "generateSinus", "chart_printing", "generateRectangular", "generateCosine", "generateSawtooth"]
+__all__ = ["__doc__", "__version__", "generateSinus", "chart_printing", "generateRectangular", "generateCosine", "generateSawtooth", "imageFiltration"]
